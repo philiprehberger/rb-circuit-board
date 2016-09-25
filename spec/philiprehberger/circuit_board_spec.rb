8150353ba60e66148bd488f788d5d5e94845f78d
Reverend Philip <me@philiprehberger.com>
2016-09-25 00:11:17 -0600

    Release v0.1.3

--- spec/philiprehberger/circuit_board_spec.rb
+++ spec/philiprehberger/circuit_board_spec.rb
@@ -34,6 +34,15 @@ RSpec.describe Philiprehberger::CircuitBoard do
       expect(status.results.length).to eq(1)
       expect(status.results.first[:name]).to eq(:new)
     end
+
+    it 'accepts checks with custom timeouts' do
+      described_class.configure do
+        check(:db, timeout: 10) { true }
+      end
+
+      status = described_class.check
+      expect(status.results.first[:healthy]).to be true
+    end
   end
 
   describe '.check' do
@@ -49,6 +58,28 @@ RSpec.describe Philiprehberger::CircuitBoard do
       status = described_class.check
       expect(status.results).to be_empty
       expect(status.healthy?).to be true
+    end
+
+    it 'runs all configured checks' do
+      described_class.configure do
+        check(:a) { true }
+        check(:b) { true }
+        check(:c) { true }
+      end
+
+      status = described_class.check
+      expect(status.results.length).to eq(3)
+    end
+  end
+
+  describe '.reset!' do
+    it 'clears all checks' do
+      described_class.configure do
+        check(:test) { true }
+      end
+      described_class.reset!
+      status = described_class.check
+      expect(status.results).to be_empty
     end
   end
 
@@ -67,6 +98,36 @@ RSpec.describe Philiprehberger::CircuitBoard do
         Philiprehberger::CircuitBoard.configure do
           check(:a) { true }
           check(:b) { false }
+        end
+
+        expect(Philiprehberger::CircuitBoard.check.healthy?).to be false
+      end
+
+      it 'returns false when all checks fail' do
+        Philiprehberger::CircuitBoard.configure do
+          check(:a) { false }
+          check(:b) { false }
+        end
+
+        expect(Philiprehberger::CircuitBoard.check.healthy?).to be false
+      end
+
+      it 'returns true with no checks' do
+        status = Philiprehberger::CircuitBoard::Status.new([])
+        expect(status.healthy?).to be true
+      end
+
+      it 'returns true with single passing check' do
+        Philiprehberger::CircuitBoard.configure do
+          check(:only) { true }
+        end
+
+        expect(Philiprehberger::CircuitBoard.check.healthy?).to be true
+      end
+
+      it 'returns false with single failing check' do
+        Philiprehberger::CircuitBoard.configure do
+          check(:only) { false }
         end
 
         expect(Philiprehberger::CircuitBoard.check.healthy?).to be false
@@ -98,6 +159,21 @@ RSpec.describe Philiprehberger::CircuitBoard do
         end
 
         expect(Philiprehberger::CircuitBoard.check.degraded?).to be false
+      end
+
+      it 'returns true with multiple passing and one failing' do
+        Philiprehberger::CircuitBoard.configure do
+          check(:a) { true }
+          check(:b) { true }
+          check(:c) { false }
+        end
+
+        expect(Philiprehberger::CircuitBoard.check.degraded?).to be true
+      end
+
+      it 'returns false with no checks' do
+        status = Philiprehberger::CircuitBoard::Status.new([])
+        expect(status.degraded?).to be false
       end
     end
 
@@ -132,6 +208,43 @@ RSpec.describe Philiprehberger::CircuitBoard do
         result = Philiprehberger::CircuitBoard.check.to_h
         expect(result[:status]).to eq('unhealthy')
       end
+
+      it 'reports healthy status when all pass' do
+        Philiprehberger::CircuitBoard.configure do
+          check(:a) { true }
+          check(:b) { true }
+        end
+
+        result = Philiprehberger::CircuitBoard.check.to_h
+        expect(result[:status]).to eq('healthy')
+      end
+
+      it 'includes all check results' do
+        Philiprehberger::CircuitBoard.configure do
+          check(:db) { true }
+          check(:redis) { true }
+          check(:queue) { false }
+        end
+
+        result = Philiprehberger::CircuitBoard.check.to_h
+        expect(result[:checks].length).to eq(3)
+        names = result[:checks].map { |c| c[:name] }
+        expect(names).to contain_exactly(:db, :redis, :queue)
+      end
+
+      it 'returns healthy for empty checks' do
+        result = Philiprehberger::CircuitBoard.check.to_h
+        expect(result[:status]).to eq('healthy')
+        expect(result[:checks]).to be_empty
+      end
+    end
+
+    describe '#results' do
+      it 'returns the results array' do
+        results = [{ name: :test, healthy: true, duration: 0.001 }]
+        status = Philiprehberger::CircuitBoard::Status.new(results)
+        expect(status.results).to eq(results)
+      end
     end
   end
 
@@ -147,6 +260,80 @@ RSpec.describe Philiprehberger::CircuitBoard do
       check = described_class.new(:fast, timeout: 5) { true }
       result = check.call
       expect(result[:duration]).to be_a(Float)
+    end
+
+    it 'returns healthy true for passing check' do
+      check = described_class.new(:pass, timeout: 5) { true }
+      result = check.call
+      expect(result[:healthy]).to be true
+    end
+
+    it 'returns healthy false for failing check' do
+      check = described_class.new(:fail, timeout: 5) { false }
+      result = check.call
+      expect(result[:healthy]).to be false
+    end
+
+    it 'includes the check name in results' do
+      check = described_class.new(:my_check, timeout: 5) { true }
+      result = check.call
+      expect(result[:name]).to eq(:my_check)
+    end
+
+    it 'treats nil return as unhealthy' do
+      check = described_class.new(:nil_check, timeout: 5) { nil }
+      result = check.call
+      expect(result[:healthy]).to be false
+    end
+
+    it 'treats truthy return as healthy' do
+      check = described_class.new(:truthy, timeout: 5) { 'yes' }
+      result = check.call
+      expect(result[:healthy]).to be true
+    end
+
+    it 'has a name accessor' do
+      check = described_class.new(:db, timeout: 5) { true }
+      expect(check.name).to eq(:db)
+    end
+
+    it 'has a timeout accessor' do
+      check = described_class.new(:db, timeout: 3) { true }
+      expect(check.timeout).to eq(3)
+    end
+
+    it 'does not include error key for non-exception failures' do
+      check = described_class.new(:fail, timeout: 5) { false }
+      result = check.call
+      expect(result).not_to have_key(:error)
+    end
+
+    it 'includes error key only for exceptions' do
+      check = described_class.new(:err, timeout: 5) { raise 'oops' }
+      result = check.call
+      expect(result).to have_key(:error)
+      expect(result[:error]).to eq('oops')
+    end
+  end
+
+  describe Philiprehberger::CircuitBoard::Configuration do
+    it 'starts with empty checks' do
+      config = described_class.new
+      expect(config.checks).to eq([])
+    end
+
+    it 'registers a check' do
+      config = described_class.new
+      config.check(:db) { true }
+      expect(config.checks.length).to eq(1)
+    end
+
+    it 'registers multiple checks' do
+      config = described_class.new
+      config.check(:db) { true }
+      config.check(:redis) { true }
+      config.check(:queue) { true }
+      expect(config.checks.length).to eq(3)
     end
   end
 
@@ -192,6 +379,43 @@ RSpec.describe Philiprehberger::CircuitBoard do
 
       status, _headers, _body = middleware.call('PATH_INFO' => '/health')
       expect(status).to eq(503)
+    end
+
+    it 'returns 503 for /health/ready when unhealthy' do
+      Philiprehberger::CircuitBoard.configure do
+        check(:bad) { false }
+      end
+
+      status, _headers, body = middleware.call('PATH_INFO' => '/health/ready')
+      expect(status).to eq(503)
+      expect(body.first).to include('not_ready')
+    end
+
+    it 'returns JSON content type for /health/live' do
+      _status, headers, _body = middleware.call('PATH_INFO' => '/health/live')
+      expect(headers['content-type']).to eq('application/json')
+    end
+
+    it 'returns valid JSON from /health' do
+      require 'json'
+      _status, _headers, body = middleware.call('PATH_INFO' => '/health')
+      parsed = JSON.parse(body.first)
+      expect(parsed).to have_key('status')
+      expect(parsed).to have_key('checks')
+    end
+
+    it 'returns valid JSON from /health/ready' do
+      require 'json'
+      _status, _headers, body = middleware.call('PATH_INFO' => '/health/ready')
+      parsed = JSON.parse(body.first)
+      expect(parsed).to have_key('status')
+    end
+
+    it 'returns valid JSON from /health/live' do
+      require 'json'
+      _status, _headers, body = middleware.call('PATH_INFO' => '/health/live')
+      parsed = JSON.parse(body.first)
+      expect(parsed['status']).to eq('alive')
     end
   end
 end
